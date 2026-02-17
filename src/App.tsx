9b3c7048127aff69f35c79e4c065a03229409ba6
b4shk1r <azmtcnd@icloud.com>
2026-02-17 00:00:47 -0500

    feat: init repo base react vite tsx

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Carleton Match</h1>
+      </header>
+      <main>
+        <p>Welcome to Carleton Match.</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
